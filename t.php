bed0fb7423e455dbbf52359740c429e03b99b013
mytory <mytory@gmail.com>
2013-12-06 02:24:15 +0900

    change lib to imagick.

--- t.php
+++ t.php
@@ -15,28 +15,22 @@ if( isset($_GET['t']) AND ! empty($_GET['t']) ){
 	$text = $_GET['t'];
 }
 
-$color_code = array(0, 0, 0);
+$color_code = '000000';
 if( isset($_GET['c']) AND ! empty($_GET['c']) ){
 	if(strlen($_GET['c']) === 6){
-		$color_code = array(
-			hexdec(substr($_GET['c'], 0, 2)),
-			hexdec(substr($_GET['c'], 2, 2)),
-			hexdec(substr($_GET['c'], 4, 2)),
-		);
+		$color_code = $_GET['c'];
 	}
 	if(strlen($_GET['c']) === 3){
-		$color_code = array(
-			hexdec(substr($_GET['c'], 0, 1).substr($_GET['c'], 0, 1)),
-			hexdec(substr($_GET['c'], 1, 1).substr($_GET['c'], 1, 1)),
-			hexdec(substr($_GET['c'], 2, 1).substr($_GET['c'], 2, 1)),
-		);
+		$color_code = substr($_GET['c'], 0, 1).substr($_GET['c'], 0, 1) .
+			substr($_GET['c'], 1, 1).substr($_GET['c'], 1, 1) .
+			substr($_GET['c'], 2, 1).substr($_GET['c'], 2, 1);
 	}
 }
 
-$font_angle = 0;
+$image_angle = 0;
 
 // create bounding box for text (글자 들어갈 박스 크기 계산)
-$bbox = imagettfbbox($font_size, $font_angle, $font_file, $text);
+$bbox = imagettfbbox($font_size, $image_angle, $font_file, $text);
 
 $upper_left_x = $bbox[0]; // -1
 $upper_left_y = $bbox[1]; // -1
@@ -51,32 +45,39 @@ $lower_left_y = $bbox[7]; // -16
 $width_calc = $lower_right_x - $lower_left_x;
 $height_calc = $upper_left_y - $lower_left_y;
 
-$width_addition = $width_calc * 1/33;
+$width_addition = $width_calc * 1/50;
 $height_addition = $height_calc * 1/3;
 
 $width = $width_calc + $width_addition;
 $height = $height_calc + $height_addition;
 
-// canvas setting (캔버스)
-$im = imagecreatetruecolor($width, $height);
-
-// canvas font color setting (캔버스 글자색)
-$font_color = imagecolorallocate($im, $color_code[0], $color_code[1], $color_code[2]);
-
-// canvas background setting (캔버스 배경)
-$transparent = imagecolorallocate($im, 255, 255, 255);
-imagecolortransparent($im, $transparent);
-imagefilledrectangle($im, 0, 0, $width - 1, $height - 1, $transparent);
-
 // This is our cordinates for X and Y (좌표설정)
 $x = 0;
 $y = $height_calc;
 
-// Write it
-imagettftext($im, $font_size, $font_angle, $x, $y, $font_color, $font_file, $text);
 
-// Output to browser
-header('Content-Type: image/png');
+/* Create some objects */
+$image = new Imagick();
+$draw = new ImagickDraw();
+$pixel = new ImagickPixel('transparent');
 
-imagepng($im);
-imagedestroy($im);
+/* New image */
+$image->newImage($width, $height, $pixel);
+
+/* Black text */
+$draw->setFillColor('#' . $color_code);
+
+/* Font properties */
+$draw->setFont($font_file);
+$draw->setFontSize($font_size * 96/72);
+
+/* Create text */
+$image->annotateImage($draw, $x, $y, $image_angle, $text);
+
+/* Give image a format */
+$image->setImageFormat('png');
+
+/* Output the image with headers */
+header('Content-type: image/png');
+echo $image;
+exit;
